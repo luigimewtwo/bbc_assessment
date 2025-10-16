db392f1000895feb5fd3e5c6e9ce35bd96a7ac2f
luigimewrwo <harveyo1@hotmail.co.uk>
2025-10-16 18:48:50 +0100

    Successfully connected to the API

--- Assessment.py
+++ Assessment.py
@@ -1,1 +1,6 @@
-print("Hello world")
+import requests
+
+response = requests.get("https://d2ssns4w4hvcti.cloudfront.net/status",
+                        headers={"X-API-Key": "c0f729ac225ae545358ac0b0dbaea776"}
+                        )
+print (response.text)
