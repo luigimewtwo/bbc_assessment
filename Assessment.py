068cd62a24830777ede17dfcc57d275a18e727a8
luigimewrwo <harveyo1@hotmail.co.uk>
2025-10-16 18:52:35 +0100

    got the API endpoints

--- Assessment.py
+++ Assessment.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get("https://d2ssns4w4hvcti.cloudfront.net/status",
+response = requests.get("https://d2ssns4w4hvcti.cloudfront.net/openapi.yaml",
                         headers={"X-API-Key": "c0f729ac225ae545358ac0b0dbaea776"}
                         )
 print (response.text)
